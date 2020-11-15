b2076c4a0635f4c4f3efdf78ed4857ab1068372f
SarkoziMelisa <sarkozi_melisa2009@yahoo.com>
2020-11-15 15:28:17 +0200

    Lab3

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import {ItemEdit, ItemList} from './todo'
-import {ItemProvider} from './todo/ItemProvider'
-import Home from './pages/Home';
+import { ItemEdit, ItemList } from './todo';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -24,21 +22,25 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import NewItem from "./pages/NewItem";
+import { ItemProvider } from './todo/ItemProvider';
+import { AuthProvider, Login, PrivateRoute } from './auth';
 
 const App: React.FC = () => (
-  <IonApp>
-      <ItemProvider>
-          <IonReactRouter>
-              <IonRouterOutlet>
-                  <Route path="/items" component={ItemList} exact={true} />
-                  <Route path="/item" component={ItemEdit} exact={true} />
-                  <Route path="/item/:id" component={ItemEdit} exact={true} />
-                  <Route exact path="/" render={() => <Redirect to="/items" />} />
-              </IonRouterOutlet>
-          </IonReactRouter>
-      </ItemProvider>
-  </IonApp>
+    <IonApp>
+        <IonReactRouter>
+            <IonRouterOutlet>
+                <AuthProvider>
+                    <Route path="/login" component={Login} exact={true}/>
+                    <ItemProvider>
+                        <PrivateRoute path="/items" component={ItemList} exact={true}/>
+                        <PrivateRoute path="/item" component={ItemEdit} exact={true}/>
+                        <PrivateRoute path="/item/:id" component={ItemEdit} exact={true}/>
+                    </ItemProvider>
+                    <Route exact path="/" render={() => <Redirect to="/items"/>}/>
+                </AuthProvider>
+            </IonRouterOutlet>
+        </IonReactRouter>
+    </IonApp>
 );
 
 export default App;
